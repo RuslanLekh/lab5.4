f73231cd6a754ea3c9be087d8ffcccc5e0412503
RuslanLekh <ryslanleh@gmail.com>
2022-04-16 21:07:42 +0300

    first commit

---
+++ src/task.java
@@ -0,0 +1,6 @@
+public class task {
+    public static void main(String[] args) {
+        System.out.println("Батько: Лех Михайло Богданович, 1981 року народження, підприємець.");
+        System.out.println("Мати: Лех Леся Миколаївна, 1983 року народження, підприємець");
+    }
+}
